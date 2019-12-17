1598f2bec740298a6740bda11abf4b149dc7bb4b
unknown <lauren.paranhos22@gmail.com>
2019-12-17 20:17:44 -0300

    resolver exercicio 1

---
+++ resolucao.py
@@ -0,0 +1,16 @@
+texto = input('Digite um texto: ')
+
+for item in range(0, 7):
+    for item in range(6):
+        print(texto, end="")
+
+    print (texto)
+    
+##escada= ''
+##caracter= input('Caracter: ')
+#degraus= int(input('Degraus'))
+
+#for item in range(7):
+#or item in range(6)
+ #   escada += caracter
+  #  print(escada)
